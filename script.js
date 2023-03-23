811eee86095916351ce820a15e0727cab0efc737
Shubham Singh Darmwal <62296226+shubham18-hub@users.noreply.github.com>
2023-03-23 16:11:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const COMMANDS = {
     "BCA -  Bachelor of Computer Applications",
   experience:'<span class="code">Total experience: As a Content Manager in Oye Be Smartest for 3 months, Currently working as a Cyber Security Analyst in HackerBro Technologies</span>',
   certifications: 
-    "Microsoft Office Specialist PowerPoint 2016<br> Microsoft Office Specialist Word 2016<br>Microsoft Office Specialist Excel 2016<br>MTA : Networking Fundamentals<br>",
+    "Microsoft Office Specialist PowerPoint 2016<br> Microsoft Office Specialist Word 2016<br>Microsoft Office Specialist Excel 2016<br>MTA : Networking Fundamentals<br>ISEA Certified Cyber Hygiene Practitioner<br>",
   contact:
     "You can contact me on Linkedin and or via the mail:<br><a href='https://www.linkedin.com/in/shubhamsinghdarmwal/' class='success link'>Linkedin</a>, <a href='mailto:sdarmwal17@gmail.com' class='success link'>Email</a>."
 };
